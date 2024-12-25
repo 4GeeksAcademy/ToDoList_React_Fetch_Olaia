afe5be7d44b73c39ee68616067bf5472eec8da0b
entelequeia <olaia96tb@gmail.com>
2024-12-25 01:18:33 +0000

    falta pulir

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,116 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+
+  const [tareas, setTareas] = useState([])
+  const [inputValue, setInputValue] = useState("")
+  const handlechange = (e) => {
+    setInputValue(e.target.value);
+
+  };
+  const createUser = () => {
+    fetch("https://playground.4geeks.com/todo/users/OlaiaTodo", {
+      method: "POST",
+    })
+      .then((response) => {
+        if (response.status === 404) {
+          return fetch(
+            "https://playground.4geeks.com/todo/users/OlaiaTodo",
+            {
+              method: "POST",
+            }
+          );
+        }
+        return response.json();
+      })
+      .then((response) => getUser())
+      .catch((error) => console.error("Error:", error));
+  };
+
+  const getUser = () => {
+    fetch("https://playground.4geeks.com/todo/users/OlaiaTodo")
+      .then((response) => response.json())
+      .then((response) => setTareas(response.todos));
+  };
+
+  const addTask = () => {
+    const newTask = { label: inputValue, is_done: false };
+    fetch("https://playground.4geeks.com/todo/todos/OlaiaTodo", {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((response) => getUser())
+      .catch((error) => console.error(error));
+  };
+
+  const deleteTask = (id) => {
+    fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json)
+      .then((response) => getUser())
+      .cath((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    createUser();
+  }, []);
+  console.log(tareas);
+
+
+  return (
+    <div className="Container text-center">
+      <h1>Lista de tareas de Olaia</h1>
+      <div className="inputContainer">
+        <input type="text" onChange={handlechange} value={inputValue} />
+        <button className="AgregarTarea"
+          onClick={() => {
+            addTask();
+            setInput("");
+          }}
+        >
+          <b>Agregar tarea</b>
+        </button>
+      </div>
+      <ul>
+        {tareas?.map((tarea) => {
+          return <ul className="list-item mt-3">{tarea.label} <button type="button" className="btn-close mx-3" onClick={() => {
+            deleteTask(tarea.id);
+          }}
+          ></button>
+
+          </ul>;
+        })}
+      </ul>
+
+    </div>
+  );
 };
 
 export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
